faaac01905a0b1558f93a1449ff2350dc30c4e86
Jusce Cabrera <jusce240@gmail.com>
2026-03-04 16:00:37 +0100

    migrate navbar to javafx

--- src/main/java/org/example/demo2/HelloApplication.java
+++ src/main/java/org/example/demo2/HelloApplication.java
@@ -3,7 +3,10 @@ package org.example.demo2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.example.demo2.controller.NavBarController;
 import org.example.demo2.controller.PostGeneratorController;
 import org.example.demo2.dao.PostDao;
 import org.example.demo2.model.Post;
@@ -16,18 +19,23 @@ import java.util.List;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException, SQLException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/post-detail-view.fxml"));
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/post-create.fxml"));
+        VBox mainScene = new VBox();
+        NavBarController navBarController = new NavBarController();
+        PostGeneratorController postGeneratorController = new PostGeneratorController();
 
 
-//        comment je peux ajouter les donnes (for loop components comment Nextjs (data.map(item => <Component item={item} />)) en java ?
-//        Scene scene = new Scene(fxmlLoader.load(), 1440, 720);
-
+        String navbarLogoImage = HelloApplication.class.getResource("img/nova_logo_new.png").toExternalForm();
         List<Post> allPosts = PostDao.getAll();
 
-        PostGeneratorController postGeneratorController = new PostGeneratorController();
-        Scene scene = new Scene(postGeneratorController.generateAllPosts(allPosts), 1440, 720);
+
+        HBox navbar = navBarController.generateNavbar(navbarLogoImage);
+        VBox posts = postGeneratorController.generateAllPosts(allPosts);
+
+
+        mainScene.getChildren().addAll(navbar, posts);
+
+
+        Scene scene = new Scene(mainScene, 1440, 720);
         scene.getStylesheets().add(HelloApplication.class.getResource("styles.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
---
+++ src/main/java/org/example/demo2/controller/NavBarController.java
@@ -0,0 +1,38 @@
+package org.example.demo2.controller;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class NavBarController {
+    public HBox generateNavbar(String image) {
+        HBox mainBox = new HBox(400);
+        mainBox.setAlignment(Pos.valueOf("CENTER"));
+//        mainBox.setId("navbar-wrapper");
+        Insets mainBoxPadding = new Insets(20);
+        mainBox.setPadding(mainBoxPadding);
+
+        // ImageView
+        ImageView novaLogo = new ImageView();
+        Image novaLogoImage = new Image(image);
+        novaLogo.setImage(novaLogoImage);
+
+
+        // Search bar
+        TextField searchBar = new TextField();
+        searchBar.setPromptText("Search...");
+        searchBar.getStyleClass().add("navbar-searchbar");
+
+
+        // Login button
+        Button loginButton = new Button("Login");
+        loginButton.getStyleClass().add("button-login");
+
+        mainBox.getChildren().addAll(novaLogo, searchBar, loginButton);
+        return mainBox;
+    }
+}
